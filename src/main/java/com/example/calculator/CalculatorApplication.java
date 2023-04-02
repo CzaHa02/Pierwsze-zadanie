3c0c1a5d8d70360ec65425974d910c6ba8df8760
CzaHa02 <129668044+CzaHa02@users.noreply.github.com>
2023-04-02 18:57:30 +0200

    Update CalculatorApplication.java

--- src/main/java/com/example/calculator/CalculatorApplication.java
+++ src/main/java/com/example/calculator/CalculatorApplication.java
@@ -1,17 +1,18 @@
-public class CalculatorApplication {
+public class Calculator {
     static double firstNumber= 10.0;
     static double secondNumber=5.0;
 
     public static void main(String[] args) {
-        CalculatorApplication calculate = new CalculatorApplication();
+        Calculator calculate = new Calculator();
         calculate.methods();
     }
 
 
     public void methods(){
         double addition = firstNumber + secondNumber;
-        double substraction = firstNumber - secondNumber;
+        double subtraction = firstNumber - secondNumber;
         System.out.println("Addition: " + addition);
-        System.out.println("Substraction: " + substraction);
+        System.out.println("Subtraction: " + subtraction);
     }
 }
+
